9bb6f61b248e221d61a27b595ba2f8a53965b83f
Rafia <rafia.habib135@gmail.com>
2025-09-05 14:51:26 +0500

    updated url

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://careercraft-frontend-zeta.vercel.app',
+  origin: 'https://backend-forever-zeta.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
